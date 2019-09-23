8547f8d7e41b914fc4bf156a5360aa9757d5ecdb
attilav <attila.varjas@gmail.com>
2019-09-23 09:29:01 +0700

    post delete, update

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,5 +7,13 @@
     <div>
         {{$post->body}}
     </div>
+    <hr>
     <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+    {!! Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('DELETE', ['class' => 'btn btn-danger'])}}
+    {!! Form::close() !!}
 @endsection('')
